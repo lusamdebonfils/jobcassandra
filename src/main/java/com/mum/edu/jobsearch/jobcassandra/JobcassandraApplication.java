e0b4f0acd0a9a809d427aad06b22c9d16db84d34
lusamdebonfils <samuelluswata@gmail.com>
2020-01-29 01:53:15 -0600

    Initial commit

---
+++ src/main/java/com/mum/edu/jobsearch/jobcassandra/JobcassandraApplication.java
@@ -0,0 +1,13 @@
+package com.mum.edu.jobsearch.jobcassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobcassandraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobcassandraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mum/edu/jobsearch/jobcassandra/JobcassandraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mum.edu.jobsearch.jobcassandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobcassandraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
